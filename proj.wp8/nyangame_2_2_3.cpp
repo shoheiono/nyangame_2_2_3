b51314a6a8cea71564cfe0d72dec8bd53bf64acf
shohei ono <wa2.a.nek0ch+git@gmail.com>
2014-04-20 12:15:14 +0900

    create nyangame by cocos2d-x ver.2.2.3

---
+++ proj.wp8/nyangame_2_2_3.cpp
@@ -0,0 +1,149 @@
+﻿#include <wrl/client.h>
+#include <d3d11_1.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <agile.h>
+#include <ppltasks.h>
+#include "nyangame_2_2_3.h"
+#include "CCApplication.h"
+
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace Windows::Phone::UI::Input;
+using namespace Windows::Graphics::Display;
+using namespace concurrency;
+USING_NS_CC;
+
+nyangame_2_2_3::nyangame_2_2_3()
+{
+}
+
+void nyangame_2_2_3::Initialize(CoreApplicationView^ applicationView)
+{
+	applicationView->Activated +=
+		ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &nyangame_2_2_3::OnActivated);
+
+	CoreApplication::Suspending +=
+		ref new EventHandler<SuspendingEventArgs^>(this, &nyangame_2_2_3::OnSuspending);
+
+	CoreApplication::Resuming +=
+		ref new EventHandler<Platform::Object^>(this, &nyangame_2_2_3::OnResuming);
+}
+
+void nyangame_2_2_3::SetWindow(CoreWindow^ window)
+{
+    // Specify the orientation of your application here
+    // The choices are DisplayOrientations::Portrait or DisplayOrientations::Landscape or DisplayOrientations::LandscapeFlipped
+	DisplayProperties::AutoRotationPreferences = DisplayOrientations::Landscape;
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &nyangame_2_2_3::OnVisibilityChanged);
+
+	window->Closed += 
+		ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &nyangame_2_2_3::OnWindowClosed);
+
+	window->PointerPressed +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &nyangame_2_2_3::OnPointerPressed);
+
+	window->PointerMoved +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &nyangame_2_2_3::OnPointerMoved);
+
+	window->PointerReleased +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &nyangame_2_2_3::OnPointerReleased);
+
+    CCEGLView* eglView = new CCEGLView();
+	eglView->Create(window);
+    eglView->setViewName("nyangame_2_2_3");
+}
+
+void nyangame_2_2_3::Load(Platform::String^ entryPoint)
+{
+}
+
+void nyangame_2_2_3::Run()
+{
+    CCApplication::sharedApplication()->run();
+}
+
+void nyangame_2_2_3::Uninitialize()
+{
+}
+
+void nyangame_2_2_3::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnVisibilityChanged(sender, args);
+}
+
+void nyangame_2_2_3::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnWindowClosed(sender, args);
+}
+
+void nyangame_2_2_3::OnPointerPressed(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerPressed(sender, args);
+}
+
+void nyangame_2_2_3::OnPointerMoved(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerMoved(sender, args);
+}
+
+void nyangame_2_2_3::OnPointerReleased(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerReleased(sender, args);
+}
+
+void nyangame_2_2_3::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	HardwareButtons::BackPressed += ref new EventHandler<BackPressedEventArgs^>(this, &nyangame_2_2_3::OnBackButtonPressed);   
+	CoreWindow::GetForCurrentThread()->Activate();
+}
+
+void nyangame_2_2_3::OnBackButtonPressed(Object^ sender, BackPressedEventArgs^ args)
+{
+    // Leave args->Handled set to false and the app will quit when user presses the back button on the phone
+}
+
+void nyangame_2_2_3::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+{
+	// Save app state asynchronously after requesting a deferral. Holding a deferral
+	// indicates that the application is busy performing suspending operations. Be
+	// aware that a deferral may not be held indefinitely. After about five seconds,
+	// the app will be forced to exit.
+	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
+	//m_renderer->ReleaseResourcesForSuspending();
+
+	create_task([this, deferral]()
+	{
+		// Insert your code here.
+
+		deferral->Complete();
+	});
+}
+ 
+void nyangame_2_2_3::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+{
+	// Restore any data or state that was unloaded on suspend. By default, data
+	// and state are persisted when resuming from suspend. Note that this event
+	// does not occur if the app was previously terminated.
+	// m_renderer->CreateWindowSizeDependentResources();
+}
+
+IFrameworkView^ Direct3DApplicationSource::CreateView()
+{
+	return ref new nyangame_2_2_3();
+}
+
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+	auto direct3DApplicationSource = ref new Direct3DApplicationSource();
+	CoreApplication::Run(direct3DApplicationSource);
+	return 0;
+}
